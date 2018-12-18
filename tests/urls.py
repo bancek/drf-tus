332da60da300f98ad8d857d98b18ce78dfff4bff
Luka Zakrajsek <luka@bancek.net>
2018-12-18 10:11:09 +0100

    Fix Django 2 urls app_name

--- tests/urls.py
+++ tests/urls.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8
 from __future__ import unicode_literals, absolute_import
 
+import django
 from django.conf.urls import url, include
 
 from rest_framework_tus.urls import urlpatterns as rest_framework_tus_urls
 
-urlpatterns = [
-    url(r'^', include(rest_framework_tus_urls, namespace='rest_framework_tus')),
-]
+if django.VERSION >= (2, 0):
+    urlpatterns = [
+        url(r'^', include((rest_framework_tus_urls, 'tests'), namespace='rest_framework_tus')),
+    ]
+else:
+    urlpatterns = [
+        url(r'^', include(rest_framework_tus_urls, namespace='rest_framework_tus')),
+    ]
